6c143716d0336355e0e59e8314568b44ddf09fe8
Vince-Ai <vinceai@outlook.com>
2020-08-04 22:09:58 -0700

    Add save file
    Add save file function

--- SocialAffordance/DataAPI.py
+++ SocialAffordance/DataAPI.py
@@ -2,6 +2,9 @@ import socket
 import os
 import collections
 import json
+import threading
+import time
+from pyautogui import press
 import numpy as np
 
 #Joint Type
@@ -256,8 +259,23 @@ class MayaController:
         for joints, attrs in data["objects"].items():
             for attr_name, value in attrs["attrs"].items():
                 if isinstance(value["value"], float):
-                    self.SetObjectAttribute(joints, attr_name, value["value"])
+                    self.SetObjectAttribute(joints, attr_name, value["value"])        
+        self.saveFile(saving_path)
         
-        send_message = "file -rename \"%s\"; file -save -type \"mayaBinary\"" % saving_path
-        rec_message = self.SendCommand(send_message)
-        print(rec_message)
+    def saveFile(self, saving_path: str):
+        '''
+        Save file to the specified directory
+        '''
+        def save(saving_path):
+            send_message = "file -rename \"%s\"; file -save -type \"mayaBinary\"" % saving_path
+            rec_message = self.SendCommand(send_message)
+            return rec_message
+        
+        save_thread = threading.Thread(target=save, args=(saving_path,))
+        save_thread.start()
+        time.sleep(1)
+        press("enter")
+        time.sleep(1)
+        
+        
+        
